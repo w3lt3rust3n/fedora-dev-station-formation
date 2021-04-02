68de05be2225fe7638aba06f1aaf96e02f69f449
weltrusten <weltrusten@localhost.localdomain>
2021-04-02 16:55:59 +0200

    updated version of the batch of the day

---
+++ php/player/formulaire.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="" method="POST" enctype="multipart/form-data">
+        <input type="file" name="mp3">
+        <input type="file" name="cover">
+        <input type="text" name="title">
+        <input type="text" name="artiste">
+        <input type="text" name="genre">
+        <input type="text">
+        <input type="text">
+        <select name="" id="">
+            <option value=""></option>
+            <option value=""></option>
+        </select>
+    </form>
+</body>
+</html>
---
+++ php/player/lecteur.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>player audio</title>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+    <script src="./assets/js/jquery.color.min.js"></script>
+    <script src="./assets/js/script.js"></script>
+    <link rel="stylesheet" href="./assets/css/style.css">
+</head>
+
+<body>
+    <div id="lecteur">
+        <div id="cover">
+            <img src="" alt="">
+        </div>
+        <div id="controller">
+            <div id="prev"><img src="./assets/img/step-backward-solid.svg" alt=""></div>
+            <div id="playPause"><img src="./assets/img/play-circle-solid.svg" alt=""></div>
+            <div id="next"><img src="./assets/img/step-forward-solid.svg" alt=""></div>
+        </div>
+        <div id="volume">
+            <div id="down"><img src="./assets/img/volume-down-solid.svg" alt=""></div>
+            <div id="up"><img src="./assets/img/volume-up-solid.svg" alt=""></div>
+        </div>
+        <div id="time"></div>
+        <div id="message"></div>
+    </div>
+</body>
+
+</html>
